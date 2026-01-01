0de830edc6ffc0b07b95284f545ffe7d7c37dfb8
Jonas Bernoulli <jonas@bernoul.li>
2026-01-01 19:45:28 +0100

    Release version 1.2.9

--- orglink.el
+++ orglink.el
@@ -7,7 +7,7 @@
 ;; Homepage: https://github.com/tarsius/orglink
 ;; Keywords: hypermedia
 
-;; Package-Version: 1.2.8
+;; Package-Version: 1.2.9
 ;; Package-Requires: (
 ;;     (emacs  "26.1")
 ;;     (compat "30.1")
